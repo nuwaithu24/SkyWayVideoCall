ee95b38493c5847cf8602ab8673f3d221ffabdf2
nuwaithu24 <nuwaithu90@gmail.com>
2023-01-30 12:01:33 +0900

    add

--- Example/SkyWayVideoCall/ViewController.swift
+++ Example/SkyWayVideoCall/ViewController.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
-import SkyWayVideoCall
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let lib = showAlert()
-        lib.printOutPut(text: "HELLO")
+        
+        
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ SkyWayVideoCall/Classes/libraryFunction.swift
@@ -0,0 +1,19 @@
+//
+//  libraryFunction.swift
+//  Pods-SkyWayVideoCall_Example
+//
+//  Created by Nu Wai Thu on 2023/01/30.
+//
+
+import Foundation
+
+public class LibraryFunction{
+    
+    public init (){
+        
+    }
+    
+    public func showOutPut(test:String){
+        print(test)
+    }
+}
